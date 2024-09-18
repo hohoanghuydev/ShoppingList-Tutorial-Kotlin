4b294b62d5db9b296e235f1d66b18e29532ec886
Hồ Hoàng Huy <hohoanghuy2603@gmail.com>
2024-09-18 15:02:56 +0700

    setup database room
    create shoppingitem entity, dao, database

---
+++ app/src/main/java/com/example/shoppinglist_toturialkotlin/data/database/daos/ShoppingItemDao.kt
@@ -0,0 +1,19 @@
+package com.example.shoppinglist_toturialkotlin.data.database.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.shoppinglist_toturialkotlin.data.database.entities.ShoppingItem
+
+interface ShoppingItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun deleteItem(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAll() : LiveData<List<ShoppingItem>>
+}
